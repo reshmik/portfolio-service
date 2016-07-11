a2d720f35abdc4f946ef86105f7d521adc0d3ca9
Reshmi Krishna <rkrishna@pivotal-guest-5-6.ny.pivotallabs.com>
2016-07-11 12:00:16 -0400

    updating to scs 1.1.0 and brixton sr1

--- src/main/java/io/pivotal/portfolio/PortfolioApplication.java
+++ src/main/java/io/pivotal/portfolio/PortfolioApplication.java
@@ -4,16 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * SpringBoot application for the portfolio microservice.
  * 
  * Responsible for managing the portfolio as well as providing the API.
  * 
- * @author David Ferreira Pinto
+ * @author Reshmi Krishna
  *
  */
 @SpringBootApplication
@@ -22,10 +24,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableCircuitBreaker
 public class PortfolioApplication {
 	
+	
 	@Bean
-	public Sampler<?> defaultSampler() {
-		return new AlwaysSampler();
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
+
+		@Bean
+		public AlwaysSampler defaultSampler() {
+			return new AlwaysSampler();
+		}
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
